a51b6c5504d3760416cc46f16d33d2937cf902c2
Javier Oswaldo <javiersacor201430061@cunoc.edu.gt>
2021-06-23 12:55:48 -0600

    clase trasgo

---
+++ src/anillos/bestias/Trasgo.java
@@ -0,0 +1,13 @@
+package anillos.bestias;
+
+import anillos.Bestia;
+
+public class Trasgo extends Bestia 
+{
+
+    public Trasgo(String nombre) 
+    {
+        super(nombre, 325);
+    }
+    
+}
