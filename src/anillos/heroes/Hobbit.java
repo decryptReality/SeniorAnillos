1e212579e3b77929e8458d8cf9f92d0b0011d8e5
Javier Oswaldo <javiersacor201430061@cunoc.edu.gt>
2021-06-23 12:56:43 -0600

    clase heroes

---
+++ src/anillos/heroes/Hobbit.java
@@ -0,0 +1,12 @@
+package anillos.heroes;
+
+import anillos.Heroe;
+
+public class Hobbit extends Heroe
+{
+
+    public Hobbit(String nombre) 
+    {
+        super(nombre, 200);
+    }
+}
---
+++ src/anillos/heroes/Humano.java
@@ -0,0 +1,13 @@
+package anillos.heroes;
+
+import anillos.Heroe;
+
+public class Humano extends Heroe 
+{
+
+    public Humano(String nombre) 
+    {
+        super(nombre, 180);
+    }
+    
+}
