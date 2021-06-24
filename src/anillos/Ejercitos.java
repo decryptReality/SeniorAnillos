51802f920a9006bd54c3d588ae5e8aeacc754021
Javier Oswaldo <javiersacor201430061@cunoc.edu.gt>
2021-06-23 20:41:41 -0600

    clase ejercito

--- src/anillos/Ejercitos.java
+++ src/anillos/Ejercitos.java
@@ -1,5 +1,7 @@
 package anillos;
 
+import java.util.Scanner;
+
 import anillos.bestias.Orco;
 import anillos.bestias.Trasgo;
 import anillos.heroes.Elfo;
@@ -8,12 +10,36 @@ import anillos.heroes.Humano;
 
 public class Ejercitos 
 {
+
+    public static void main(String[] args) 
+    {
+        Ejercitos ejercitos = new Ejercitos(5);
+        ejercitos.turnos(true);
+    }
+
+    static Scanner scanner = new Scanner(System.in);
     int elementos;
     Bestia[] bestias = {};
     Heroe[] heroes;
 
+    Ejercitos(int elementos)
+    {
+        this.elementos = elementos;
+        bestias = new Bestia[this.elementos];
+        heroes = new Heroe[this.elementos];
+        generarEjercitos();
+    }
 
-    Bestia getBestia(int caso, int indice)
+    void generarEjercitos()
+    {
+        for(int i = 0; i < elementos; i = i + 1)
+        {
+            bestias[i] = generarBestia(Extras.dado(1), i);
+            heroes[i] = generarHeroe(Extras.dado(2), i);
+        }
+    }
+
+    Bestia generarBestia(int caso, int indice)
     {
         if(caso == 0)
         {
@@ -26,7 +52,7 @@ public class Ejercitos
         return null;
     }
 
-    Heroe getHeroe(int caso, int indice)
+    Heroe generarHeroe(int caso, int indice)
     {
         if(caso == 0)
         {
@@ -43,21 +69,104 @@ public class Ejercitos
         return null;
     }
 
-    void generarEjercitos()
+    void turnos(Boolean empiezaBestia)
     {
-        for(int i = 0; i < elementos; i = i + 1)
+        int salir = 0;
+        int bestiasVivas = elementos;
+        int heroesVivos = elementos;
+        while(0 != bestiasVivas | 0 != heroesVivos | -1 != salir)
         {
-            bestias[i] = getBestia(Extras.dado(1), i);
-            heroes[i] = getHeroe(Extras.dado(2), i);
+            if (empiezaBestia) 
+            {
+                turnoBestia();
+                bestiasVivas = bestiasVivas();
+                heroesVivos = heroesVivos();
+                if(0 != bestiasVivas | 0 != heroesVivos)
+                {
+                    turnoHeroe();
+                    bestiasVivas = bestiasVivas();
+                    heroesVivos = heroesVivos();
+                }
+            } 
+            else 
+            {
+                turnoHeroe();
+                bestiasVivas = bestiasVivas();
+                heroesVivos = heroesVivos();
+                if(0 != bestiasVivas | 0 != heroesVivos)
+                {
+                    turnoBestia();
+                    bestiasVivas = bestiasVivas();
+                    heroesVivos = heroesVivos();
+                }
+            }
+            System.out.println("Ingrese -1 para salir o 1 para continuar");
+            salir = scanner.nextInt();
+        }
+        System.out.println("Bestias vivas: " + bestiasVivas);
+        System.out.println("Heroes vivos: " + heroesVivos);
+        String ganador = bestiasVivas > heroesVivos ? "bestias" : "heroes";
+        System.out.println("[?] Ganaron: " + ganador);
+    }
+
+    void turnoBestia()
+    {
+        for (int i = 0; i < elementos; i = i + 1) 
+        {
+            // si ambos tienen vida
+            if(0 < bestias[i].getVida() & 0 < heroes[i].getVida())
+            {
+                // heroe rescibe danio de bestia
+                heroes[i].danio(bestias[i]);
+            }
+            else
+            {
+                // si la bestia tiene vida y el heroe no
+                if(0 < bestias[i].getVida())
+                {
+                    // en [i+1,elementos-1] de heroes se busca uno con vida
+                    for (int j = i + 1; j < elementos; j = j + 1) 
+                    {
+                        if (0 < heroes[j].getVida()) 
+                        {
+                            // y se ataca
+                            heroes[j].danio(bestias[i]);
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 
-    Ejercitos(int elementos)
+    void turnoHeroe()
     {
-        this.elementos = elementos;
-        bestias = new Bestia[this.elementos];
-        heroes = new Heroe[this.elementos];
-        generarEjercitos();
+        for (int i = 0; i < elementos; i = i + 1) 
+        {
+            // si ambos tienen vida
+            if(0 < heroes[i].getVida() & 0 < bestias[i].getVida())
+            {
+                // bestia recibe danio de heroe
+                bestias[i].danio(heroes[i]);
+            }    
+            else 
+            {
+                // si heroe tiene vida y bestia no
+                if (0 < heroes[i].getVida()) 
+                {
+                    // en [i+1,elementos-1] de bestias se busca uno con vida
+                    for (int j = i + 1; j < elementos; j = j + 1) 
+                    {
+                        if (0 < bestias[j].getVida()) 
+                        {
+                            // bestia recibe danio de heroe
+                            bestias[j].danio(heroes[i]);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
     }
 
     int bestiasVivas()
@@ -85,93 +194,6 @@ public class Ejercitos
             }
         }
         return vivos;
-    }
-
-    boolean ejecutar(boolean empiezaBestias)
-    {
-        int bestias = 0;
-        int heroes = 0;
-        while (bestias != 0 | heroes != 0)
-        {
-            if(empiezaBestias)
-            {
-                turno(true);
-                turno(false);
-            }
-            else
-            {
-                turno(false);
-                turno(true);
-            }
-            bestias = bestiasVivas();
-            heroes = heroesVivos();
-        }
-        return bestias > heroes ? true : false;
-    }
-
-    void turno(boolean esTurnoBestias)
-    {
-        if (esTurnoBestias)
-        {
-            for (int i = 0; i < elementos; i = i + 1) 
-            {
-                // si ambos tienen vida
-                if(0 < heroes[i].getVida() & 0 < bestias[i].getVida())
-                {
-                    heroes[i].danio(bestias[i]);
-                }
-                else
-                {
-                    // si la bestia tiene vida y el heroe no
-                    if(0 < bestias[i].getVida())
-                    {
-                        // [i+1,elementos-1]
-                        for (int j = i + 1; j < elementos; j = j + 1) 
-                        {
-                            if (0 < heroes[j].getVida()) 
-                            {
-                                heroes[j].danio(bestias[i]);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-        else
-        {
-            for (int i = 0; i < elementos; i = i + 1) 
-            {
-                // si ambos tienen vida
-                if(0 < heroes[i].getVida() & 0 < bestias[i].getVida())
-                {
-                    bestias[i].danio(heroes[i]);
-                }    
-                else 
-                {
-                    // si heroe tiene vida y bestia no
-                    if (0 < heroes[i].getVida()) 
-                    {
-                        // [i+1,elementos-1]
-                        for (int j = i + 1; j < elementos; j = j + 1) 
-                        {
-                            if (0 < bestias[j].getVida()) 
-                            {
-                                bestias[j].danio(bestias[i]);
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    void ataque(Bestia bestia, Heroe heroe)
-    {
-        heroe.danio(bestia);
-    }
-    void ataque(Heroe heroe, Bestia bestia)
-    {
-        bestia.danio(heroe);
     }
 
 }
